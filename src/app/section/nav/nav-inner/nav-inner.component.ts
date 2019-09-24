ad4ee6666f323f01bee82b462cdebecb73b5dd5e
Samuel Reid <samuelreid@samuels-mbp.duke-energy.com>
2019-09-24 19:01:35 -0400

    finishng up parent/child data

--- src/app/section/nav/nav-inner/nav-inner.component.ts
+++ src/app/section/nav/nav-inner/nav-inner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav-inner',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-inner.component.css']
 })
 export class NavInnerComponent implements OnInit {
+
+  @Input() ParentData: string;
+
+  @Output() childEvent = new EventEmitter;
+
+  onChange(value) {
+    this.childEvent.emit(value);
+  }
 
   constructor() { }
 
